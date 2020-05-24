ce4460cfdb95b85b23f34166bcd87c5183ed8dc9
Fabio <soaresfabiojr@gmail.com>
2020-05-24 12:03:10 +1200

    simplify player function

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     var totalTime = 0
     var secondsPassed = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var titleLabel: UILabel!
     
@@ -45,31 +46,10 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleLabel.text = "Done!"
-            playSound()
-        }
-    }
-    
-    var player: AVAudioPlayer?
-    
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-            guard let player = player else { return }
-
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
         }
     }
     
